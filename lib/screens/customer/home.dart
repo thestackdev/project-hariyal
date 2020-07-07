6b142c8f857b8b65fe1ecfcbeb7bbd67e19427b4
GauthamAsir <gauthamasir@gmail.com>
2020-07-07 22:18:51 +0530

    Show Products & Product Details
    Signed-off-by: GauthamAsir <gauthamasir@gmail.com>

--- lib/screens/customer/home.dart
+++ lib/screens/customer/home.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:the_project_hariyal/screens/customer/models/product_model.dart';
 import 'package:the_project_hariyal/screens/customer/models/user_model.dart';
+import 'package:the_project_hariyal/screens/customer/network_image.dart';
+import 'package:the_project_hariyal/screens/customer/product_details.dart';
 
 class Home extends StatefulWidget {
   final UserModel userModel;
@@ -19,6 +21,86 @@ class _HomeState extends State<Home> {
 
   _HomeState(this.userModel);
 
+  String stateCategory, stateValue;
+  String areaCategory, areaValue;
+
+  List<DocumentSnapshot> products = [];
+
+  @override
+  void initState() {
+    setState(() {
+      stateCategory = 'location.state';
+      stateValue = userModel.location['state'];
+      areaCategory = 'location.area';
+      areaValue = userModel.location['cityDistrict'];
+    });
+    super.initState();
+  }
+
+  void showFilterDialog() {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Filter'),
+            content: Wrap(
+              children: [
+                Column(
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          'Area',
+                          textScaleFactor: 1.4,
+                        ),
+                        new DropdownButton<String>(
+                          items: <String>['hyderabad', 'secunderabad']
+                              .map((String value) {
+                            return new DropdownMenuItem<String>(
+                              value: value,
+                              child: new Text(value),
+                            );
+                          }).toList(),
+                          onChanged: (newValue) {
+                            setState(() {
+                              areaCategory = 'location.area';
+                              areaValue = newValue;
+                            });
+                          },
+                        )
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          'State',
+                          textScaleFactor: 1.4,
+                        ),
+                        new DropdownButton<String>(
+                          items: <String>['andhra pradesh', 'telangana']
+                              .map((String value) {
+                            return new DropdownMenuItem<String>(
+                              value: value,
+                              child: new Text(value),
+                            );
+                          }).toList(),
+                          onChanged: (newValue) {
+                            setState(() {
+                              stateCategory = 'location.state';
+                              stateValue = newValue;
+                            });
+                          },
+                        )
+                      ],
+                    )
+                  ],
+                )
+              ],
+            ),
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,7 +108,9 @@ class _HomeState extends State<Home> {
         title: Text('Home'),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () {
+              showFilterDialog();
+            },
             icon: Icon(Icons.filter_list),
           )
         ],
@@ -107,25 +191,112 @@ class _HomeState extends State<Home> {
         ),
       ),
       body: SafeArea(
-        child: StreamBuilder<QuerySnapshot>(
+        child: StreamBuilder(
           stream: Firestore.instance.collection('products').snapshots(),
           builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return ListView.builder(
-                  itemCount: snapshot.data.documents.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    ProductModel productModel = ProductModel.fromMap(
-                        snapshot.data.documents[index].data,
-                        snapshot.data.documents[index].documentID);
-                    return Text(productModel.id);
-                  });
-            } else {
+            if (snapshot.data == null ||
+                snapshot.connectionState == ConnectionState.none ||
+                snapshot.connectionState == ConnectionState.waiting) {
               return Center(
                 child: SpinKitWave(
-                    color: Colors.orangeAccent, type: SpinKitWaveType.start),
+                  color: Colors.orange,
+                  size: 50.0,
+                ),
               );
             }
+            if (snapshot.hasError) {
+              return Center(
+                child: Text(
+                  snapshot.error,
+                  textScaleFactor: 2,
+                ),
+              );
+            }
+            if (!snapshot.hasData || snapshot.data.documents.length <= 0) {
+              return Center(
+                child: Text(
+                  'No Products Available',
+                  textScaleFactor: 2,
+                ),
+              );
+            }
+            return ListView.builder(
+                itemCount: snapshot.data.documents.length,
+                itemBuilder: (context, index) {
+                  ProductModel productModel = ProductModel.fromMap(
+                      snapshot.data.documents[index].data,
+                      snapshot.data.documents[index].documentID);
+                  return buildItems(context, productModel);
+                });
           },
+        ),
+      ),
+    );
+  }
+
+  Widget buildItems(BuildContext context, ProductModel productModel) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (_) {
+          return ProductDetail(productModel);
+        }));
+      },
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 18, vertical: 12),
+        child: Card(
+          elevation: 6,
+          child: Padding(
+            padding: EdgeInsets.all(12),
+            child: Row(
+              children: [
+                Container(
+                  height: 120,
+                  width: MediaQuery.of(context).size.width / 3,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.all(Radius.circular(6)),
+                    child: Hero(
+                      tag: productModel.id,
+                      child: PNetworkImage(
+                        productModel.images[0],
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ListTile(
+                        title: Text(
+                          productModel.title,
+                          style: TextStyle(fontSize: 21),
+                        ),
+                        subtitle: Text(
+                          productModel.description,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: 20),
+                        child: Text(
+                          'Price',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.favorite_border),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
---
+++ lib/screens/customer/product_details.dart
@@ -0,0 +1,161 @@
+import 'package:flutter/material.dart';
+import 'package:the_project_hariyal/screens/customer/image_slider.dart';
+import 'package:the_project_hariyal/screens/customer/models/product_model.dart';
+
+class ProductDetail extends StatefulWidget {
+  final ProductModel productModel;
+
+  ProductDetail(this.productModel);
+
+  @override
+  _ProductDetailState createState() => _ProductDetailState(productModel);
+}
+
+class _ProductDetailState extends State<ProductDetail> {
+  final ProductModel productModel;
+
+  _ProductDetailState(this.productModel);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+              height: MediaQuery.of(context).size.height / 1.5,
+              width: MediaQuery.of(context).size.width,
+              child: ImageSliderWidget(
+                imageUrls: productModel.images,
+                imageBorderRadius: BorderRadius.circular(8.0),
+              )),
+          SafeArea(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: Row(
+                    children: [
+                      MaterialButton(
+                        padding: const EdgeInsets.all(8.0),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0)),
+                        child: Icon(Icons.arrow_back_ios),
+                        color: Colors.white,
+                        textColor: Colors.black,
+                        minWidth: 0,
+                        height: 40,
+                        onPressed: () => Navigator.pop(context),
+                      ),
+                    ],
+                  ),
+                ),
+                Spacer(),
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(32.0),
+                        color: Colors.white),
+                    child: Column(
+                      children: [
+                        const SizedBox(height: 30.0),
+                        Expanded(
+                          child: SingleChildScrollView(
+                            physics: BouncingScrollPhysics(),
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                ListTile(
+                                  title: Text(
+                                    productModel.title,
+                                    style: TextStyle(
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 28.0),
+                                  ),
+                                  trailing: IconButton(
+                                    icon: Icon(Icons.favorite_border),
+                                    onPressed: () {},
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 16.0, vertical: 12.0),
+                                  child: Text(
+                                    productModel.description,
+                                    style: TextStyle(
+                                        color: Colors.black, fontSize: 18),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 12, horizontal: 24),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(20.0),
+                                topRight: Radius.circular(20.0)),
+                            color: Colors.grey.shade900,
+                          ),
+                          child: Row(
+                            children: [
+                              Text(
+                                'Price' + " Rs",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18.0),
+                              ),
+                              const SizedBox(width: 20.0),
+                              Spacer(),
+                              RaisedButton(
+                                padding: const EdgeInsets.symmetric(
+                                    vertical: 8.0, horizontal: 16.0),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10.0)),
+                                onPressed: () async {},
+                                color: Colors.orange,
+                                textColor: Colors.white,
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: <Widget>[
+                                    Text(
+                                      "Book Now",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 16.0),
+                                    ),
+                                    const SizedBox(width: 20.0),
+                                    Container(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Icon(
+                                        Icons.arrow_forward_ios,
+                                        color: Colors.orange,
+                                        size: 16.0,
+                                      ),
+                                      decoration: BoxDecoration(
+                                          color: Colors.white,
+                                          borderRadius:
+                                              BorderRadius.circular(10.0)),
+                                    )
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
